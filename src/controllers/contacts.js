68c4b626b33f7fb907ba95d1c0d8e4022dbc6366
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-02-02 15:18:29 +0000

    add 4 homework

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,5 @@
 import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 import {
   createContact,
@@ -7,10 +8,23 @@ import {
   getContactById,
   updateContact,
 } from '../services/contacts.js';
-import mongoose from 'mongoose';
+
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
 
   res.json({
     status: 200,
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,26 @@
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isContactType = (contactType) =>
+    ['work', 'home', 'personal'].includes(contactType);
+
+  if (isContactType(contactType)) return contactType;
+};
+
+const parseIsFavourite = (isFavourite) => {
+  const isString = typeof isFavourite === 'string';
+  if (!isString) return;
+  if (isFavourite === 'true') return true;
+  if (isFavourite === 'false') return false;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+
+  const parsedContactType = parseContactType(contactType);
+
+  return {
+    contactType: parsedContactType,
+    isFavourite: parseIsFavourite(isFavourite),
+  };
+};
